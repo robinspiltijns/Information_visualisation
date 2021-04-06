3b25f5a7b78078428c537124c4d4f280eac96a91
robinspitlijns <spiltijnsrobin@gmail.com>
2021-04-06 14:28:31 +0200

    small refactor

--- index.js
+++ index.js
@@ -11,43 +11,42 @@ let body = d3.select("body");
 
 let svg = body.append("svg").attr("width", width).attr("height", height);
 
-d3.json("data/data.json").then(function (data) {
+d3.json("data/data.json").then((data) => {
     let matrix = [];
     let nodes = data.entities
-        .filter((e) => e.type == "entitlement")
-        .map((e) => {return {id: e.id, name: e.name}});
-    
-    nodes.forEach((n, i) => {n.index = i;});
+        .filter((e) => e.type === "entitlement")
+        .map((e, i) => {return {id: e.id, name: e.name, index: i}});
 
-    let total_items = nodes.length;
-
-    let links = data.relationships
-        .filter((r) => (r.fromEntityType == "user" && r.toEntityType == "entitlement"))
-        .map((r) => {return {source: r.fromEntityId, target: r.toEntityId}})
+    let amountOfNodes = nodes.length;
 
     nodes.forEach((node) => {
-        matrix[node.index] = d3.range(total_items).map(item_index => {
+        matrix[node.index] = d3.range(amountOfNodes).map(item_index => {
             return {
                 x: item_index,
                 y: node.index,
                 z: 0
             };
         });
-    })
+    });
 
-    links.forEach((link1) => {
-        let user = link1.source;
-        let node1_index = nodes.filter((n) => n.id == link1.target)[0].index;
-        links.filter((l) => l.source == user)
-             .map((l) => (nodes.filter((n) => n.id == l.target)[0].index))
-             .filter((node2_index) => node2_index != node1_index)
+    let links = data.relationships
+        .filter((r) => (r.fromEntityType === "user" && r.toEntityType === "entitlement"))
+        .map((r) => {return {source: r.fromEntityId, target: r.toEntityId}});
+
+    links.forEach((link) => {
+        let userId = link.source;
+        let entitlementId = link.target
+        let node1_index = nodes.filter((node) => node.id === entitlementId)[0].index;
+        links.filter((link) => link.source === userId)
+             .map((link) => (nodes.filter((node) => node.id === link.target)[0].index))
+             .filter((node2_index) => node2_index !== node1_index)
              .forEach((node2_index) => {
                 matrix[node1_index][node2_index].z += 1;
         });
     });
 
-    let matrixScale = d3.scaleBand().range([0, width]).domain(d3.range(total_items));
-    var opacityScale = d3.scaleLinear().domain([0, 5]).range([0.0, 1.0]).clamp(true);
+    let matrixScale = d3.scaleBand().range([0, width]).domain(d3.range(amountOfNodes));
+    let opacityScale = d3.scaleLinear().domain([0, 5]).range([0.0, 1.0]).clamp(true);
 
     let rows = svg.selectAll('.row')
         .data(matrix)
@@ -57,7 +56,7 @@ d3.json("data/data.json").then(function (data) {
             return "translate(0," + matrixScale(i) + ")";
         });
 
-    
+
     let squares = rows.selectAll(".cell")
         .data(d => d)
         .enter().append("rect")
@@ -66,7 +65,7 @@ d3.json("data/data.json").then(function (data) {
         .attr("width", matrixScale.bandwidth())
         .attr("height", matrixScale.bandwidth())
         .style("fill", "blue")
-        .style("fill-opacity", d => opacityScale(d.z))
+        .style("fill-opacity", d => opacityScale(d.z));
 
     let columns = svg.selectAll(".column")
         .data(matrix)
@@ -80,3 +79,4 @@ d3.json("data/data.json").then(function (data) {
     console.log(links)
     console.log(matrix)
 });
+
